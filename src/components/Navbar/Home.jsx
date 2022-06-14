60cc3bbcb779b461c0bb857e241c01b8efbf5294
khansConstant <huzaifakahn.98@gamil.com>
2022-06-15 03:26:34 +0530

    first commit

--- src/components/Navbar/Home.jsx
+++ src/components/Navbar/Home.jsx
@@ -6,7 +6,7 @@ export default function
     return (
         <div className='hero'>
             <div class="card bg-dark text-white border-0">
-                <img src="/assests/1.jpg" class="card-img" alt="Background" height="550px" />
+                <img src="1.jpg" class="card-img" alt="Background" height="550px" />
                 <div className="container">
                     <div class="card-img-overlay d-flex flex-column justify-content">
                         <h5 class="card-title display-3 fw-bolder mb-0">NEW ARRIVALS</h5>
